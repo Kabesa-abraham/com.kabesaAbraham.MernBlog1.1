c67dc258ef6b64efe1c501d1cca8695e247adc1a
Kabesa-abraham <yebulaabraham@gmail.com>
2024-10-18 18:21:26 +0100

    creation des pages et des différents routes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Projects from "./Pages/Projects";
 
 function App() {
   return (
-    <div className="" >
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/projects" element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
