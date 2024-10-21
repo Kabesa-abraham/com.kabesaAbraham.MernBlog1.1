44a609f50f26e4889c9670db2a336774673e6f71
Kabesa-abraham <yebulaabraham@gmail.com>
2024-10-21 21:17:17 +0100

    Création d'une connexion avec la base de données MONGO-DB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require('express')
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{console.log('MongoDB connected!')})
+                                       .catch((err) => {console.log(err);})
 
 const app = express();
 
