3e712f83d1455e8481cee50a1eb4398a0dcc92b8
Kabesa-abraham <yebulaabraham@gmail.com>
2024-10-21 20:27:01 +0100

    Creation d'un navbar responsize
    Dans cette partie j'ai crée un navbar(Header) complètement responsive.

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Dashboard from "./Pages/Dashboard";
-import About from "./Pages/About";
-import SignIn from "./Pages/SignIn";
-import SignUp from "./Pages/SignUp";
-import Projects from "./Pages/Projects";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Projects from './Pages/Projects';
+import Dashboard from './Pages/Dashboard';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/projects" element={<Projects/>} />
-      </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+       <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/projects' element={<Projects/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ frontEnd/src/Components/Header/Header.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react'
+import './header.css'
+import { Link } from 'react-router-dom'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon} from 'react-icons/fa'
+import {MdClose, MdMenu} from 'react-icons/md'
+
+const Header = () => {
+
+  const [active,setActive] = useState(false)
+  const HandleShopOption = () =>{
+    if(active === false){
+      setActive(true);
+    }else{
+      setActive(false)
+  }}
+ const change= () =>{ //je l'ai mis dans les differents options pour cacher la barre des options
+  setActive(false)
+ }
+
+
+  return (
+   <header className='flex justify-between items-center relative p-3 border-b-2'>
+
+    <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold' >
+      <span className='px-2 py-1 bg-gradient-to-r from-blue-600 via-indigo-600 to-pink-500 text-white
+        rounded-lg 
+      ' >Abram's</span>Blog
+    </Link>
+
+    <form 
+      className='bg-gray-100 px-4 py-3 rounded-lg hidden lg:flex items-center search' 
+     >
+      <input type='text' placeholder='Rechercher...' className='bg-transparent border-none outline-none'/>
+      <AiOutlineSearch className='text-xl' />
+    </form>
+    <button className='w-12 h-10 flex justify-center items-center lg:hidden text-lg search_btn2' >
+      <AiOutlineSearch/>
+    </button>
+
+    <div className='flex items-center gap-3 md:order-2' >
+      <button  className='w-12 h-10 hidden sm:flex justify-center items-center search_btn2' > {/*j'ai aussi mis search_btn2' pour utilisé son style */}
+        <FaMoon/>
+      </button>
+
+      <Link to='/sign-in' >
+        <button 
+          className='bg-gradient-to-br from-indigo-700 to-blue-400 text-white font-semibold px-2 py-2 rounded-lg' >
+          Se Connecter
+        </button>
+      </Link>
+
+      {
+        active ? <button className='w-11 h-9 flex justify-center items-center md:hidden menu_btn' onClick={HandleShopOption}>
+        <MdClose className='text-2xl ' />
+        </button> :
+        <button className='w-11 h-9 flex justify-center items-center md:hidden menu_btn' onClick={HandleShopOption}>
+        <MdMenu className='text-2xl ' />
+        </button>
+      }
+    </div>
+
+    <div className={active?'flex items-center gap-5 option_container' : 'flex items-center gap-5 option_container active'}  >
+        <Link to='/' >
+          <button className='option_btn' onClick={change}>
+            Home
+          </button>
+        </Link>
+        <Link to='/about' >
+          <button className='option_btn' onClick={change}>
+            About
+          </button>
+        </Link>
+        <Link to='/projects' >
+          <button className='option_btn' onClick={change}>
+            Projects
+          </button>
+        </Link>
+     </div>
+
+   </header>
+  )
+}
+
+export default Header
