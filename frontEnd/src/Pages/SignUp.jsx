9a25226d089062c3ed8f6a7f04f94a9b83b341e5
Kabesa-abraham <yebulaabraham@gmail.com>
2024-10-22 21:58:01 +0100

    Ajout des fonctionnalité à la page Sign up
    Pour la création de compte utilisateur.

--- frontEnd/src/Pages/SignUp.jsx
+++ frontEnd/src/Pages/SignUp.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Style/sign_in_up.css'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import { Alert, Button,CircularProgress } from '@mui/material'
+import { MdError } from "react-icons/md";
 
 const SignUp = () => {
+
+  const [formData,setFormData] = useState({}) //ce useState va récuperer les differents valeur des champs
+  const [errorMessage, setErrorMessage] = useState(null) // ceci va être utilisé si vous avez une erreur
+  const [loading, setLoading] = useState(false) // ceci va nous permettre de faire un effet d'attente
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{  //cette fonction va me permettre de prendre les valeurs de mes différents champs
+    setFormData({...formData , [e.target.id] : e.target.value.trim()}) // ici j'ai utilisé l'id mais on peut aussi utiliser l'attribut name
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage("S'il vous plait remplissez tout les champs")
+    }
+
+    try{
+      setLoading(true)
+
+      setErrorMessage(null) //pour nettoyer cette state
+      const res =  await fetch('/backend/auth/signup' ,{
+        method:'POST',
+        headers:{ "Content-Type" : 'application/json' },
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+
+      if(data.success === false){ //cette erreur va s'afficher si les données sont déjà existant dans la base de données
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false)
+
+      if(res.ok){ // si tout va très bien alors on va aller à la page sign in
+        navigate('/sign-in')
+      }
+
+    }catch(error){
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
   return (
-    <div className='min-h-screen mt-20' >
+    <div className='min-h-screen mt-20 theContainer' >
       <div className='flex p-3 max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-8' >
         {/*left side*/}
         <div className='flex-1' >
@@ -22,35 +69,49 @@ const SignUp = () => {
         {/*right side */}
         <div className='flex-1' >
           <div>
-            <form className='flex flex-col' >
+            <form className='flex flex-col' onSubmit={handleSubmit} >
               
               <div className='fieldcontain' >
                 <label htmlFor="username">Votre Nom d'utilisateur</label>
-                <input type="text" placeholder="Nom d'utilisateur" id='username'/>
+                <input type="text" placeholder="Nom d'utilisateur" id='username' onChange={handleChange}/>
               </div>
+
               <div className='fieldcontain' >
                 <label htmlFor="email">Votre Email</label>
-                <input type="email" placeholder="nom@gmail.com" id='email'/>
+                <input type="email" placeholder="nom@gmail.com" id='email' onChange={handleChange}/>
               </div>
+
               <div className='fieldcontain' >
                 <label htmlFor="password">Votre Mot de passe</label>
-                <input type="password" placeholder="Mot de passe" id='password'/>
-              </div>
-              <button type='submit' 
-                      className='bg-gradient-to-r from from-blue-600 via-purple-600 to-pink-500 py-3 hover:bg-gradient-to-l
-                                text-white font-semibold rounded-xl mt-4'
-               >
-                S'INSCRIRE
-              </button>
-
-              <div className='flex gap-2 text-sm mt-5 font-semibold ' >
-                <span>Vous avez déjà un Compte?</span>
-                <Link to='/sign-in' className='text-blue-500 hover:underline' >
-                  Connectez vous
-                </Link>
+                <input type="password" placeholder="Mot de passe" id='password' onChange={handleChange}/>
               </div>
 
+              <Button className='bg-gradient-to-r from from-blue-600 via-purple-600 to-pink-500 signup_btn' type='submit' disabled={loading}> {/*J'ai désactiver le bouton lorque le loading est true */}
+                { loading ? (
+                    <>
+                      <CircularProgress size={25} className='circularprogress'/>
+                      <span className='lowercase text-sm text-white' >En attente...</span>
+                    </>
+                  ) : "Sign Up"
+                }
+              </Button>
+             
             </form>
+
+            <div className='flex gap-2 text-sm mt-5 font-semibold ' >
+              <span>Vous avez déjà un Compte?</span>
+              <Link to='/sign-in' className='text-blue-500 hover:underline' >
+                 Connectez vous
+              </Link>
+            </div>
+
+            {
+              errorMessage && (  /*cette alert va apparaitre si vous n'avez une erreur selon la logique de la fonction mis ci-haut */
+                <Alert className='mt-5' color='error' icon={<MdError/>} >
+                  {errorMessage}
+                </Alert>)
+            }
+
           </div>
         </div>
       </div>
--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{ //je l'ai mis manuellement pour ne plus écrire http ect...
+    proxy:{
+      '/backend':{
+        target:'http://localhost:3000',
+        secure:false
+      },
+    },
+  },
   plugins: [react()],
 })
