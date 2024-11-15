60f7e89f452dfb9e541fd74bfec17fc4227d8f45
Kabesa-abraham <yebulaabraham@gmail.com>
2024-11-15 15:08:23 +0100

    changement 3

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{ //je l'ai mis manuellement pour ne plus écrire http ect...
-    proxy:{
-      '/backend':{
-        target:'http://localhost:3000',
-        secure:false
-      },
-    },
+    proxy:{},
   },
   plugins: [react()],
 })
+
+/*
+ '/backend':{
+        target:'http://localhost:3000',
+        secure:false
+      },
+*/
